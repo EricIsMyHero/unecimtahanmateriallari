dcdf668e8bde148006b6c58d7ce78f09d69841fc
EricIsMyHero <ericismyhero2467@gmail.com>
2026-04-22 22:53:07 +0400

    Update app.js

--- app.js
+++ app.js
@@ -255,7 +255,8 @@ const data = {
         semester: 1,
         pdfs: [
           { name: "Mülki Müdafiə Q26", file: "mulkimudafieQ26.pdf" },
-          { name: "Mülki Müdafiə Y24", file: "mulkimudafieY24.pdf" }
+          { name: "Mülki Müdafiə Y24", file: "mulkimudafieY24.pdf" },
+          { name: "Mülki Müdafiə Q23", file: "mulkimudafieQ23.pdf" }
         ]
       },
       "Statistika": {
